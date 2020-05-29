fd9053f4b434b60c92ac80e66ed02239cb26cb47
Quentin Crida <qcrida@gmail.com>
2020-05-29 06:05:36 +0100

    Delete & PostMapping added to TuttiController

--- musicianService/src/main/java/com/example/philharmonic/violinservice/controllers/TuttiController.java
+++ musicianService/src/main/java/com/example/philharmonic/violinservice/controllers/TuttiController.java
@@ -2,6 +2,7 @@ package com.example.philharmonic.violinservice.controllers;
 
 import com.example.philharmonic.violinservice.models.Tutti;
 import com.example.philharmonic.violinservice.repositories.TuttiRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +19,24 @@ public class TuttiController {
     @GetMapping(value ="/tuttis")
     public ResponseEntity<List<Tutti>> getAllTuttis(){return new ResponseEntity<>( tuttiRepository.findAll(),HttpStatus.OK);}
 
-    @GetMapping(value = "tuttis/{id}")
+    @GetMapping(value = "/tuttis/{id}")
     public ResponseEntity getTutti(@PathVariable Long id){ return new ResponseEntity(tuttiRepository.findById(id), HttpStatus.OK );}
 
     @PostMapping(value = "/tuttis")
     public ResponseEntity<Tutti> createTutti(@RequestBody Tutti tutti){
         tuttiRepository.save(tutti);
         return new ResponseEntity<>(tutti, HttpStatus.CREATED);
+    }
+    @PatchMapping(value = "/tuttis/{id}")
+    public ResponseEntity <Tutti> updateTutti(@RequestBody Tutti tutti){
+        tuttiRepository.save(tutti);
+        return new ResponseEntity<>(tutti, HttpStatus.OK);
+    }
+    @DeleteMapping(value = "/tuttis/{id}")
+    public ResponseEntity<Tutti> deleteTutti(@PathVariable Long id){
+        Tutti found = tuttiRepository.getOne(id);
+        tuttiRepository.delete(found);
+        return  new ResponseEntity<>(null, HttpStatus.OK);
     }
 
 }
