6dfbd5bf2b5f039af1c7eef78293fd6d0b3b8ae3
Quentin Crida <qcrida@gmail.com>
2020-06-11 06:56:24 +0100

    Musician List sorted alpahabetically

--- all_musicians_start_point/src/components/musicians/MusicianList.js
+++ all_musicians_start_point/src/components/musicians/MusicianList.js
@@ -6,25 +6,28 @@ const MusicianList = (props) => {
 	if(props.musicians.length === 0){
 		return (<p>Tuning musicians...</p>)
 	}
+	const sortedMusicians = props.musicians.sort(function(a, b){
+	 var lastNameA=a.lastName.toLowerCase(), lastNameB=b.lastName.toLowerCase();
+	 if (lastNameA < lastNameB) //sort string ascending
+	  return -1;
+	 if (lastNameA > lastNameB)
+	  return 1;
+	 return 0; //default return value (no sorting)
+	});
+
 
 
 const musicians = props.musicians.map((musician, index) => {
-// 	props.musicians.sort(function(a, b){
-//  var lastNameA=a.lastName.toLowerCase(), lastNameB=b.lastName.toLowerCase();
-//  if (lastNameA < lastNameB) //sort string ascending
-//   return -1;
-//  if (lastNameA > lastNameB)
-//   return 1;
-//  return 0; //default return value (no sorting)
-// });
+
 	return (
-		<li key={index} className="component-item">
+		<li key={index} className="component-item" >
 		<div className="component">
 		<Musician musician={musician} />
 		</div>
 		</li>
 	)
 })
+
 
 
 	return (
